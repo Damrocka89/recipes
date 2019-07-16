f6d2f88c68929d96a8eee1cd08d8737a67622644
ewkra <EW.KRASOWSKA@HOTMAIL.COM>
2019-07-16 18:28:45 +0200

    Initial commit

---
+++ src/main/java/com/ewelina/recipes/domain/Recipe.java
@@ -0,0 +1,15 @@
+package com.ewelina.recipes.domain;
+
+public class Recipe {
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    //todo add
+    //priv Diff diff
+    private Byte[] image;
+}
